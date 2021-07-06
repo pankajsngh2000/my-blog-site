c837cad4399dc02526c0519dc0d26045fc7c3740
Pankaj <pankajsngh2000@gmail.com>
2021-07-06 12:50:21 +0530

    Update settings.py
    some security fix :)

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mr02jb+)e)2+(ofn982^(z20x1zef^2!7ov=lr%r2$3s=4w2e%'
+SECRET_KEY = 'BLAHBLAH!!!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
